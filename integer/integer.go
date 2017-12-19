494a220811f7db7db46539070946452282d88d4e
xuxiaoyu2010 <xuxiaoyu2010@gmail.com>
2017-12-19 16:54:03 +0800

    add integer main

---
+++ integer/integer.go
@@ -0,0 +1,11 @@
+package integer
+
+
+func Min(x, y int) int {
+	if x > y {
+		return y
+	} else {
+		return x
+	}
+
+}
--- slices/slices.go
+++ slices/slices.go
@@ -1,22 +1,29 @@
 package slices
 
+import(
+	"goutil/integer"
+)
+
 func ChunkStringSlice( ss []string, size int ) ( [][]string, int ) {
 
 	var (
-		sursor int = 0
+		cursor int = 0
 		end = size
-		length := len( ss )
-		pice := length / size + 1
+		length = len( ss )
+		pice = length / size + 1
 		chunk [][]string
 	)
 
 	for i:=0; i < pice ; i++ {
 		
-		end = min( end, length )
-		chunk = append( chunk, ss[sursor:end] )
+		end = integer.Min( end, length )
+		chunk = append( chunk, ss[cursor:end] )
 		cursor = end
 		end = end + size
 	}
 
-	return chunk
+	return chunk, pice
 }
+
+
+
